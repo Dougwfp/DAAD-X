ecad0c2189f6b290a632651795771879483588b0
douglas willian <dougdwfp@gmail.com>
2019-05-30 20:54:37 -0300

    primiero

---
+++ secao05/DAAD/DAAD/DAAD.MobileAppService/Models/Item.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace DAAD.Models
+{
+    public class Item
+    {
+        public string Id { get; set; }
+
+        [Required]
+        public string Text { get; set; }
+
+        [Required]
+        public string Description { get; set; }
+    }
+}
---
+++ secao05/DAAD/DAAD/DAAD/Models/Tarefa.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DAAD.Models
+{
+    class Tarefa
+    {
+        public int id { get; set; }
+        public string nome { get; set; }
+        public string texto { get; set; }
+        public string resultado { get; set; }
+        public int pontuacao { get; set; }
+        public int idUsuario { get; set; }
+    }
+}
